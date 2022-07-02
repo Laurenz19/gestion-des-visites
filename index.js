b9dc8308dd3a9d6f88a0c96819ae1ed428204a8a
Laurenz19 <laurenziosambany@gmail.com>
2022-07-02 15:32:04 +0300

    making basic Crud & test the swagger docs

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const swaggerUI = require("swagger-ui-express")
+const swaggerJsDoc = require("swagger-jsdoc")
+const low  = require("lowdb")
+const visitorRoutes = require('./src/router/visitor')
+
+//db
+const FileSync = require("lowdb/adapters/FileSync")
+const adapter = new FileSync("db.json")
+const db = low(adapter)
+db.defaults({visitors:[]}).write()
+
+
+const app = express()
+app.db = db
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan("dev"))
+
+
+//routes
+app.use('/api/visitors', visitorRoutes)
+
+
+//swagger documentations configuration
+const options = {
+    definition:{
+        openapi:"3.0.0",
+        info: {
+            title:"Gestion des visites touristiques",
+            version:"1.0.0",
+            description: "RestFul Api pour la gestion des visites touristiques"
+        },
+        servers:[
+            { url: "http://localhost:5000"}
+        ]
+    },
+    apis: ["./src/router/*.js"]
+}
+
+const specs = swaggerJsDoc(options)
+app.use('/swagger', swaggerUI.serve, swaggerUI.setup(specs))
+
+
+app.listen(5000,()=>{
+    console.log('I am listen to you brother')
+})
---
+++ src/router/visitor.js
@@ -0,0 +1,220 @@
+const express = require('express')
+const nanoid = require('nanoid')
+
+
+const router = express.Router()
+
+
+const idlength = 5
+
+
+
+/**
+ * @swagger
+ * components: 
+ *  schemas:
+ *      Visitor:
+ *          type: object
+ *          required:
+ *              - name
+ *              - address
+ *          properties:
+ *              id: 
+ *                  type: string
+ *                  description: vistor's id (auto generate)
+ *              name:
+ *                  type: string
+ *                  description: the visitor's complete name
+ *              address:
+ *                  type: string
+ *                  description: the visitor's address
+ *          example:
+ *              id: "V-0001"
+ *              name: "Sambany Michel Laurenzio"
+ *              address: "Tanambao, Fianarantsoa"
+ *                  
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Visitors
+ *  description: The visitor managing API
+ */
+
+/**
+ * @swagger
+ * /api/visitors:
+ *  get:
+ *      summary: Returns the list of visitors 
+ *      tags: [Visitors]
+ *      responses:
+ *          '200':
+ *              description: A list of visitors
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/Visitor'
+ */
+
+router.get("/", (req, res)=>{
+    const visitors = req.app.db.get("visitors")
+    res.send(visitors)
+})
+
+
+/**
+ * @swagger
+ * /api/visitors/{id}:
+ *  get:
+ *      summary: Get the visitor by id
+ *      tags: [Visitors]
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          schema:         
+ *              type: string
+ *          required: true
+ *          description: The visitor id
+ *      responses:
+ *          '200':
+ *              description: The visitor description by id
+ *              content: 
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Visitor'
+ *          '404':
+ *              description: The visitor was not found
+ *               
+ */
+
+router.get("/:id", (req, res)=>{
+    const visitor = req.app.db.get("visitors").find({id: req.params.id}).value()
+    if(!visitor){
+        res.sendStatus(404)
+    }
+    res.send(visitor)
+})
+
+
+/**
+ * @swagger
+ * /api/visitors:
+ *  post:
+ *      summary: Create new visitor
+ *      tags: [Visitors]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/Visitor'
+ *      responses:
+ *          '200':
+ *              description: The visitor was successfully created
+ *              content: 
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Visitor'
+ *          '500':
+ *              description: Some server error
+ */
+router.post("/", (req, res)=>{
+    try {
+        const visitor = {
+            id:nanoid(idlength),
+            ...req.body
+        }
+        
+        req.app.db.get("visitors").push(visitor).write()
+        res.status(200).send(visitor)
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+})
+
+
+/**
+ * @swagger
+ * /api/visitors/{id}:
+ *  put:
+ *      description: Update the visitor by id
+ *      tags: [Visitors]
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: string
+ *            required: true
+ *            description: The visitor's id
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json: 
+ *                  schema:
+ *                      $ref: '#/components/schemas/Visitor'
+ *      responses:
+ *          '200':
+ *              description: The visitor was succesfully updated
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Visitor'
+ *          '404':
+ *              description: The visitor was not found
+ *          '500':
+ *              description: Some server error
+ */
+
+router.put("/:id", (req, res)=>{
+    try {
+        const visitor = req.app.db.get("visitors").find({id: req.params.id})
+
+        if(!visitor.value()){
+            res.sendStatus(404)
+        }else{
+            visitor.assign(req.body).write()
+            res.send(req.app.db.get("visitors").find({id: req.params.id}))
+        }
+        
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+})
+
+/**
+ * @swagger
+ * /api/visitors/{id}:
+ *  delete:
+ *      summary: Delete The visitor by id
+ *      tags: [Visitors]
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: string
+ *            required: true
+ *            description: The visitor's id
+ *      responses:
+ *          '200':
+ *              description: The visitor was successfully deleted
+ *          '404':
+ *              description: The visitor was not found
+ */
+router.delete("/:id", (req, res)=>{
+
+    const visitor = req.app.db.get("visitors").find({id: req.params.id})
+
+    if(!visitor.value()){
+        res.sendStatus(404)
+    }else{
+        req.app.db.get("visitors").remove({id:req.params.id}).write()
+        res.status(200).send("success")
+    }
+})
+
+
+
+module.exports = router
